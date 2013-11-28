78421d8084bae213062d8bc8967d0599b1898ff9
Nils Maier <maierman@web.de>
2013-11-28 07:43:59 +0100

    Support video Identify via ffmpeg

--- util/Ffmpeg.cs
+++ util/Ffmpeg.cs
@@ -23,7 +23,8 @@ namespace NMaier.SimpleDlna.Utilities
     };
     private readonly static LeastRecentlyUsedDictionary<FileInfo, IDictionary<string, string>> infoCache = new LeastRecentlyUsedDictionary<FileInfo, IDictionary<string, string>>(500);
     private static readonly Regex RegLine = new Regex(@"^(?:ID|META)_([\w\d_]+)=(.+)$", RegexOptions.Compiled);
-
+    private static readonly Regex RegDuration = new Regex(@"Duration: ([0-9]{2}):([0-9]{2}):([0-9]{2})(?:\.([0-9]+))?", RegexOptions.Compiled);
+    private static readonly Regex RegDimensions = new Regex(@"Video: .+ ([0-9]{2,})x([0-9]{2,}) ", RegexOptions.Compiled);
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Fidentify")]
     public static readonly string FFidentifyExecutable = FindExecutable("ffidentify");
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Fmpeg")]
@@ -58,6 +59,17 @@ namespace NMaier.SimpleDlna.Utilities
 
     public static IDictionary<string, string> IdentifyFile(FileInfo file)
     {
+      if (FFmpeg.FFidentifyExecutable != null) {
+        return IdentifyFileInternalFFidentify(file);
+      }
+      if (FFmpeg.FFmpegExecutable != null) {
+        return IdentifyFileInternalFFmpeg(file);
+      }
+      throw new NotSupportedException();
+    }
+
+    private static IDictionary<string, string> IdentifyFileInternalFFidentify(FileInfo file)
+    {
       if (FFmpeg.FFidentifyExecutable == null) {
         throw new NotSupportedException();
       }
@@ -68,43 +80,118 @@ namespace NMaier.SimpleDlna.Utilities
       if (infoCache.TryGetValue(file, out rv)) {
         return rv;
       }
-      lock (infoCache) {
-        try {
-          using (var p = new Process()) {
-            var sti = p.StartInfo;
+      try {
+        using (var p = new Process()) {
+          var sti = p.StartInfo;
 #if !DEBUG
-            sti.CreateNoWindow = true;
+          sti.CreateNoWindow = true;
 #endif
-            sti.UseShellExecute = false;
-            sti.FileName = FFmpeg.FFidentifyExecutable;
-            sti.Arguments = String.Format("\"{0}\"", file.FullName);
-            sti.LoadUserProfile = false;
-            sti.RedirectStandardOutput = true;
-            p.Start();
-            if (p.WaitForExit(2000) && p.ExitCode == 0) {
-              rv = new Dictionary<string, string>();
-              string line;
-              for (line = p.StandardOutput.ReadLine(); line != null; line = p.StandardOutput.ReadLine()) {
-                var m = RegLine.Match(line.Trim());
-                if (m.Success) {
-                  rv.Add(m.Groups[1].Value, m.Groups[2].Value);
-                }
+          sti.UseShellExecute = false;
+          sti.FileName = FFmpeg.FFidentifyExecutable;
+          sti.Arguments = String.Format("\"{0}\"", file.FullName);
+          sti.LoadUserProfile = false;
+          sti.RedirectStandardOutput = true;
+          p.Start();
+          if (p.WaitForExit(2000) && p.ExitCode == 0) {
+            rv = new Dictionary<string, string>();
+            string line;
+            for (line = p.StandardOutput.ReadLine(); line != null; line = p.StandardOutput.ReadLine()) {
+              var m = RegLine.Match(line.Trim());
+              if (m.Success) {
+                rv.Add(m.Groups[1].Value, m.Groups[2].Value);
               }
-              line = p.StandardOutput.ReadToEnd();
-              if (line != null) {
-                var m = RegLine.Match(line.Trim());
-                if (m.Success) {
-                  rv.Add(m.Groups[1].Value, m.Groups[2].Value);
-                }
-              }
-              infoCache.Add(file, rv);
-              return rv;
             }
+            line = p.StandardOutput.ReadToEnd();
+            if (line != null) {
+              var m = RegLine.Match(line.Trim());
+              if (m.Success) {
+                rv.Add(m.Groups[1].Value, m.Groups[2].Value);
+              }
+            }
+            lock (infoCache) {
+              infoCache.Add(file, rv);
+            }
+            return rv;
           }
         }
-        catch (Exception ex) {
-          throw new NotSupportedException(ex.Message, ex);
+      }
+      catch (Exception ex) {
+        throw new NotSupportedException(ex.Message, ex);
+      }
+      throw new NotSupportedException();
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "ffmpeg")]
+    private static IDictionary<string, string> IdentifyFileInternalFFmpeg(FileInfo file)
+    {
+      if (FFmpeg.FFmpegExecutable == null) {
+        throw new NotSupportedException();
+      }
+      if (file == null) {
+        throw new ArgumentNullException("file");
+      }
+      IDictionary<string, string> rv;
+      if (infoCache.TryGetValue(file, out rv)) {
+        return rv;
+      }
+      try {
+        using (var p = new Process()) {
+          var sti = p.StartInfo;
+#if !DEBUG
+          sti.CreateNoWindow = true;
+#endif
+          sti.UseShellExecute = false;
+          sti.FileName = FFmpeg.FFmpegExecutable;
+          sti.Arguments = String.Format("-i \"{0}\"", file.FullName);
+          sti.LoadUserProfile = false;
+          sti.RedirectStandardError = true;
+          p.Start();
+          rv = new Dictionary<string, string>();
+
+          using (var reader = new StreamReader(new MemoryStream())) {
+            using (var pump = new StreamPump(p.StandardError.BaseStream, reader.BaseStream, null, 4096)) {
+              if (!p.WaitForExit(2000)) {
+                throw new NotSupportedException("ffmpeg timed out");
+              }
+              if (!pump.Wait(1000)) {
+                throw new NotSupportedException("ffmpeg pump timed out");
+              }
+              reader.BaseStream.Seek(0, SeekOrigin.Begin);
+
+              var output = reader.ReadToEnd();
+              var match = RegDuration.Match(output);
+              if (match != null && match.Success) {
+                int h, m, s, ms;
+                if (int.TryParse(match.Groups[1].Value, out h) &&
+                  int.TryParse(match.Groups[2].Value, out m) &&
+                  int.TryParse(match.Groups[3].Value, out s)) {
+                  if (match.Groups.Count < 5 || !int.TryParse(match.Groups[4].Value, out ms)) {
+                    ms = 0;
+                  }
+                  var ts = new TimeSpan(0, h, m, s, ms * 10);
+                  var tss = ts.TotalSeconds.ToString(CultureInfo.InvariantCulture);
+                  rv.Add("LENGTH", tss);
+                }
+              }
+              match = RegDimensions.Match(output);
+              if (match != null && match.Success) {
+                int w, h;
+                if (int.TryParse(match.Groups[1].Value, out w) &&
+                  int.TryParse(match.Groups[2].Value, out h)) {
+                  rv.Add("VIDEO_WIDTH", w.ToString());
+                  rv.Add("VIDEO_HEIGHT", h.ToString());
+                }
+              }
+            }
+          }
+          if (rv.Count == 0) {
+            throw new NotSupportedException("File not supported");
+          }
+          return rv;
         }
+      }
+      catch (Exception ex) {
+        throw new NotSupportedException(ex.Message, ex);
       }
       throw new NotSupportedException();
     }
