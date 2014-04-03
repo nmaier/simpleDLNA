fc4949df253ec67de4b927c17d95b650a1300600
Nils Maier <maierman@web.de>
2014-04-03 17:33:58 +0200

    Add NgenInstaller

---
+++ NgenInstaller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace NMaier.SimpleDlna
+{
+  [RunInstaller(true)]
+  public class NgenInstaller : Installer
+  {
+    public override void Install(IDictionary stateSaver)
+    {
+      base.Install(stateSaver);
+      using (var proc = new Process()) {
+        proc.StartInfo.FileName = Path.Combine(RuntimeEnvironment.GetRuntimeDirectory(), "ngen.exe");
+        proc.StartInfo.Arguments = string.Format("install /nologo \"{0}\"", Assembly.GetExecutingAssembly().Location);
+        proc.StartInfo.UseShellExecute = false;
+        proc.StartInfo.CreateNoWindow = true;
+        proc.Start();
+        proc.WaitForExit();
+        if (proc.ExitCode != 0) {
+          throw new Exception("Failed to run ngen");
+        }
+      }
+    }
+  }
+}
