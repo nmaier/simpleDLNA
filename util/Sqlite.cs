c430ff631d1a40990d482c98347569d3d2a863f6
Nils Maier <maierman@web.de>
2014-08-07 08:56:33 +0200

    Update Sqlite

--- util/Sqlite.cs
+++ util/Sqlite.cs
@@ -52,31 +52,7 @@ namespace NMaier.SimpleDlna.Utilities
       rv.Open();
 
       try {
-        // XXX Try to set SQLITE_FCNTL_CHUNK_SIZE by messy reflection trick, pending
-        // ticket http://system.data.sqlite.org/index.html/tktview?name=d1c008fa0a
-        var field = typeof(System.Data.SQLite.SQLiteConnection).GetField(
-          "_sql",
-          BindingFlags.NonPublic | BindingFlags.Instance
-          );
-        var sql = field.GetValue(rv);
-        if (sql == null) {
-          throw new Exception("Failed to get field");
-        }
-        var func = sql.GetType().GetMethod(
-          "FileControl",
-          BindingFlags.NonPublic | BindingFlags.Instance
-          );
-        if (func == null) {
-          throw new Exception("Failed to get func");
-        }
-        var arg = Marshal.AllocHGlobal(sizeof(Int32));
-        try {
-          Marshal.WriteInt32(arg, GROW_SIZE);
-          func.Invoke(sql, new object[] { null, (int)0x6, arg });
-        }
-        finally {
-          Marshal.FreeHGlobal(arg);
-        }
+        rv.SetChunkSize(GROW_SIZE);
       }
       catch (Exception ex) {
         log4net.LogManager.GetLogger(typeof(Sqlite)).Error("Failed to sqlite control", ex);
