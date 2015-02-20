1465645d1d0d0e7a77ebfa38b55511add1575e21
Nils Maier <maierman@web.de>
2015-02-21 00:09:28 +0100

    Improve FilterView

--- server/Views/FilterView.cs
+++ server/Views/FilterView.cs
@@ -27,7 +27,21 @@ namespace NMaier.SimpleDlna.Server.Views
 
     protected override bool DoFilter(IMediaResource res)
     {
-      return filter.IsMatch(res.Title);
+      return filter.IsMatch(res.Title) || filter.IsMatch(res.Path);
+    }
+
+    private static string Escape(string str)
+    {
+      foreach (var c in "\\.+|[]{}()$#^".ToArray()) {
+        var cs = new string(c, 1);
+        str = str.Replace(cs, "\\" + cs);
+      }
+      if (str.Contains('*') || str.Contains("?")) {
+        str = string.Format("^{0}$", str);
+        str = str.Replace("*", ".*");
+        str = str.Replace("?", ".");
+      }
+      return str;
     }
 
     public override void SetParameters(AttributeCollection parameters)
@@ -40,16 +54,14 @@ namespace NMaier.SimpleDlna.Server.Views
         return;
       }
 
-      var f = String.Join(",", parameters.Keys);
-      f = f.Replace("*", ".*");
-      f = f.Replace("?", ".?");
-      f = f.Replace("\\", "\\\\");
-      foreach (var c in "\\+|[]{}()$#^".ToArray()) {
-        var cs = new string(c, 1);
-        f = f.Replace(cs, "\\" + cs);
-      }
-      filter = new Regex(f, RegexOptions.Compiled | RegexOptions.IgnoreCase);
-      NoticeFormat("Using filter {0}", f);
+      var filters = from f in parameters.Keys
+                    let e = Escape(f)
+                    select e;
+      filter = new Regex(
+        String.Join("|", filters),
+        RegexOptions.Compiled | RegexOptions.IgnoreCase
+      );
+      NoticeFormat("Using filter {0}", filter.ToString());
     }
 
     public override IMediaFolder Transform(IMediaFolder root)
