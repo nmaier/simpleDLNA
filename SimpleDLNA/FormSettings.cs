316b522f987eb26d17f30eeb89a161e5046658fc
Rickey Ward <RickeyWard@DiamondDrake.com>
2015-03-09 02:54:20 -0400

    Auto Start for windows added
    Added auto start option for windows GUI. in the GUI settings added
    "Start automatically with Windows" option, which creates an auto run key
    in the windows registry for the current user. Tested on windows 8. This
    uses the current running location of the GUI application so it does not
    need to be installed to work. a check for mono runtime is performed and
    it just hides the option if the GUI application is not running natively
    in Windows.

--- SimpleDLNA/FormSettings.cs
+++ SimpleDLNA/FormSettings.cs
@@ -5,10 +5,24 @@ namespace NMaier.SimpleDlna.GUI
 {
   public partial class FormSettings : NMaier.Windows.Forms.Form
   {
+
+      StartUpUtilities startUpUtilities;
+      private const string AppKeyName = "SimpleDLNA";
+
     public FormSettings()
     {
       InitializeComponent();
       Icon = Properties.Resources.preferencesIcon;
+
+      if(!StartUpUtilities.IsRunningOnMono())
+      {
+          startUpUtilities = new StartUpUtilities(false);
+          checkAutoStart.Checked = startUpUtilities.CheckIfRunAtWinBoot(AppKeyName);
+      }
+      else
+      {
+          checkAutoStart.Visible = false;
+      }
     }
 
     private void buttonBrowseCacheFile_Click(object sender, EventArgs e)
@@ -16,6 +30,18 @@ namespace NMaier.SimpleDlna.GUI
       if (folderBrowserDialog.ShowDialog() == DialogResult.OK) {
         textCacheFile.Text = folderBrowserDialog.SelectedPath;
       }
+    }
+
+    private void checkAutoStart_CheckedChanged(object sender, EventArgs e)
+    {
+        if(checkAutoStart.Checked)
+        {
+            startUpUtilities.setWinBoot(AppKeyName);
+        }
+        else
+        {
+            startUpUtilities.RemoveWinBoot(AppKeyName);
+        }
     }
   }
 }
---
+++ SimpleDLNA/StartUpUtilities.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NMaier.SimpleDlna.GUI
+{
+    class StartUpUtilities
+    {
+        RegistryKey rkApp;
+
+        public StartUpUtilities(bool AllUsers)
+        {
+            if (AllUsers)
+            {
+                // The path to the key where Windows looks for startup applications for all users
+                rkApp = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            }
+            else
+            {
+                // The path to the key where Windows looks for startup applications for single user
+                rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="AppName">"key" to search for in startup registry</param>
+        /// <returns>True if app is set to start with windows.</returns>
+        public bool CheckIfRunAtWinBoot(string AppName)
+        {
+            // Check to see the current state (running at startup or not)
+            if (rkApp.GetValue(AppName) == null)
+            {
+                // The value doesn't exist, the application is not set to run at startup
+                return false;
+            }
+
+            else
+            {
+                // The value exists, the application is set to run at startup
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Sets application to run at windows boot, using an AppName as the registry key name, and any supplied path
+        /// </summary>
+        /// <param name="AppName">Name the startup key will use, should be unique</param>
+        /// <param name="AppPath">Path to the executable to run</param>
+        public void setWinBoot(string AppName, string AppPath)
+        {
+            rkApp.SetValue(AppName, AppPath);
+        }
+
+        /// <summary>
+        /// Sets application to run at windows boot, using an AppName as the registry key name, current executable path
+        /// </summary>
+        /// <param name="AppName">Name the startup key will use, should be unique</param>
+        public void setWinBoot(string AppName)
+        {
+            rkApp.SetValue(AppName, Application.ExecutablePath);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="AppName"></param>
+        public void RemoveWinBoot(string AppName)
+        {
+            rkApp.DeleteValue(AppName, false);
+        }
+
+        /// <summary>
+        /// Returns true if applicaton is running under mono
+        /// </summary>
+        public static bool IsRunningOnMono()
+        {
+            return Type.GetType("Mono.Runtime") != null;
+        }
+    }
+}
